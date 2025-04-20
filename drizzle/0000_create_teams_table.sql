b06bcd43604f66787b161d762331544c0b7fbb73
yoshikouki <yoshikouki@pepabo.com>
2025-04-20 13:30:13 +0900

    rm -rf drizzle/

--- drizzle/0000_create_teams_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS "team" (
-	"team_id" serial PRIMARY KEY NOT NULL,
-	"name" varchar(50) NOT NULL,
-	"display_id" varchar(30) NOT NULL,
-	"created_at" timestamp DEFAULT now() NOT NULL,
-	"updated_at" timestamp DEFAULT now() NOT NULL,
-	CONSTRAINT "team_display_id_unique" UNIQUE("display_id")
-);
--- drizzle/0001_rename_primary_key.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE "team" RENAME COLUMN "team_id" TO "id";
