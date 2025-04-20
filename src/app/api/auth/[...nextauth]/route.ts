0bb5592cbfed0a6656e0f995a55b1ad43180a389
yoshikouki <yoshikouki@pepabo.com>
2025-04-20 15:50:54 +0900

    Move auth to packaged directory by feature

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,3 @@
-import { handlers } from "@/lib/auth";
+import { handlers } from "@/features/auth/config";
 
 export const { GET, POST } = handlers;
--- src/features/auth/config.ts
+++ src/features/auth/config.ts
@@ -1,8 +1,8 @@
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import NextAuth from "next-auth";
 import Discord from "next-auth/providers/discord";
-import { db } from "./db";
-import { accounts, sessions, users, verificationTokens } from "./db/schema";
+import { db } from "@/lib/db";
+import { accounts, sessions, users, verificationTokens } from "@/lib/db/schema";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: DrizzleAdapter(db, {
